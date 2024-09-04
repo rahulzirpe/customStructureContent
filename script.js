fa71c81af4263f0ca5452d100330d74782af5275
rahulzirpe <123835457+rahulzirpe@users.noreply.github.com>
2024-09-04 21:17:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -158,72 +158,56 @@ function sendQuickReplies(quickReplies) {
             var quickReply2 = document.getElementById('quickReply2').value;
             var quickReply3 = document.getElementById('quickReply3').value;
 
+            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+
             var data = {
-                text: "Please select any timeslot:",
-                quickReplies: {
-                    "type": "quickReplies",
-                    "itemsPerRow": 3,
-                    "replies": [
-                        {
-                            "type": "button",
-                            "tooltip": quickReply1,
-                            "title": quickReply1,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply1
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-12345"
-                                    }
-                                ]
+                json:{
+                    "type": "vertical",
+                    "tag": "generic",
+                    "elements": [{
+                        "type": "text",
+                        "text": text1,
+                        "tag": "title"
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply1,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply1} ${formattedDate}`
                             }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply2,
-                            "title": quickReply2,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply2
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1234"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply3,
-                            "title": quickReply3,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply3
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "No-4321"
-                                    }
-                                ]
-                            }
+                          ]
                         }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply2,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply2} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply3,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply3} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      }
+
                     ]
-                }
-            };
+            }};
             lpTag.agentSDK.command(cmdName, data, notifyWhenDone);
         }
 
@@ -234,91 +218,70 @@ function sendQuickReplies(quickReplies) {
             var quickReply3 = document.getElementById('quickReply3').value;
             var quickReply4 = document.getElementById('quickReply4').value;
 
+            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+
             var data = {
-                text: "Please select any timeslot:",
-                quickReplies: {
-                    "type": "quickReplies",
-                    "itemsPerRow": 3,
-                    "replies": [
-                        {
-                            "type": "button",
-                            "tooltip": quickReply1,
-                            "title": quickReply1,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply1
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1234"
-                                    }
-                                ]
+                json:{
+                    "type": "vertical",
+                    "tag": "generic",
+                    "elements": [{
+                        "type": "text",
+                        "text": text1,
+                        "tag": "title"
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply1,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply1} ${formattedDate}`
                             }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply2,
-                            "title": quickReply2,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply2
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1232"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply3,
-                            "title": quickReply3,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply3
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1233"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply4,
-                            "title": quickReply4,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply4
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "No-4324"
-                                    }
-                                ]
-                            }
+                          ]
                         }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply2,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply2} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply3,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply3} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply4,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply4} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      }
+
                     ]
-                }
-            };
+            }
+        };
+
             lpTag.agentSDK.command(cmdName, data, notifyWhenDone);
         }
 
@@ -330,110 +293,81 @@ function sendQuickReplies(quickReplies) {
             var quickReply4 = document.getElementById('quickReply4').value;
             var quickReply5 = document.getElementById('quickReply5').value;
 
+            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+
             var data = {
-                text: "Please select any timeslot:",
-                quickReplies: {
-                    "type": "quickReplies",
-                    "itemsPerRow": 3,
-                    "replies": [
-                        {
-                            "type": "button",
-                            "tooltip": quickReply1,
-                            "title": quickReply1,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply1
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1234"
-                                    }
-                                ]
+                json:{
+                    "type": "vertical",
+                    "tag": "generic",
+                    "elements": [{
+                        "type": "text",
+                        "text": text1,
+                        "tag": "title"
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply1,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply1} ${formattedDate}`
                             }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply2,
-                            "title": quickReply2,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply2
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1232"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply3,
-                            "title": quickReply3,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply3
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1233"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply4,
-                            "title": quickReply4,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply4
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "Yes-1234"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "type": "button",
-                            "tooltip": quickReply5,
-                            "title": quickReply5,
-                            "click": {
-                                "actions": [
-                                    {
-                                        "type": "publishText",
-                                        "text": quickReply5
-                                    }
-                                ],
-                                "metadata": [
-                                    {
-                                        "type": "ExternalId",
-                                        "id": "No-4325"
-                                    }
-                                ]
-                            }
+                          ]
                         }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply2,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply2} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply3,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply3} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply4,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply4} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "title": quickReply5,
+                        "click": {
+                          "actions": [
+                            {
+                              "type": "publishText",
+                              "text": `${quickReply5} ${formattedDate}`
+                            }
+                          ]
+                        }
+                      }
+
                     ]
-                }
-            };
+            }
+        };
             lpTag.agentSDK.command(cmdName, data, notifyWhenDone);
         }
 
